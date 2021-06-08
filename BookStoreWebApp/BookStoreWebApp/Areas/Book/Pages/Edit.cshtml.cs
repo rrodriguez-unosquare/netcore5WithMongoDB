6f736c0828995c903a70c4929fd7d4cf7ea583aa
roman rodriguez <roman.rodriguez@unosquare.com>
2021-06-08 18:25:51 -0500

    adding edit page, validations and logic to update a book

--- BookStoreWebApp/BookStoreWebApp/Areas/Book/Pages/Edit.cshtml.cs
+++ BookStoreWebApp/BookStoreWebApp/Areas/Book/Pages/Edit.cshtml.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Interfaces.Services;
 using Domain.Models.Mongo;
+using Domain.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookStoreApi.Areas.Book.Pages
 {
+    [ValidateAntiForgeryToken]
     public class EditModel : PageModel
     {
         private readonly IBookService _bookService;
-
-        public Domain.Models.Mongo.Book Book { get; set; }
 
         public EditModel(IBookService bookService)
         {
@@ -24,9 +25,54 @@ namespace BookStoreApi.Areas.Book.Pages
         [BindProperty(Name = "id", SupportsGet = true)]
         public string Id { get; set; }
 
+        [BindProperty]
+        [Required(ErrorMessage = "BookName is required")]
+        public string BookName { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+
         public void OnGet()
         {
-            this.Book = _bookService.Get(this.Id);
+            var book = _bookService.Get(this.Id);
+
+            this.Id = book.Id;
+            this.Author = book.Author;
+            this.BookName = book.BookName;
+            this.Category = book.Category;
+            this.Price = book.Price;
+
+        }
+
+        
+        public IActionResult OnPost()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var book = _bookService.Get(this.Id);
+
+            book.BookName = this.BookName;
+            book.Author = this.Author;
+            book.Price = this.Price;
+            book.Category = this.Category;
+
+            _bookService.Update(this.Id, book);
+
+            return RedirectToPage("BookStore");
 
         }
     }
