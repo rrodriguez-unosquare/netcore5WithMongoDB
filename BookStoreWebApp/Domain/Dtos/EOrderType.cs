e7a9a534f5e2f36434008f324ec3a1aa9f7f2703
roman rodriguez <roman.rodriguez@unosquare.com>
2021-06-14 15:23:05 -0500

    adding dtos

---
+++ BookStoreWebApp/Domain/Dtos/EOrderType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+    public enum  EOrderType
+    {
+        ASC = 0,
+        DESC = 1
+    }
+}
---
+++ BookStoreWebApp/Domain/Dtos/SearchBooksDto.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Models.Mongo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+    public class SearchBooksDto
+    {
+        public SearchBooksDto()
+        {
+            this.OrderBy = ESearchBookOrderBy.BookName;
+            this.OrderType = EOrderType.ASC;
+        }
+
+        public string CriteriaText { get; set; }
+
+        public int PageNumber { get; set; }
+
+
+        public int PageSize { get; set; }
+
+        public ESearchBookOrderBy OrderBy { get; set; }
+
+        public EOrderType OrderType { get; set; }
+
+    }
+
+
+    public enum ESearchBookOrderBy
+    {
+        BookName=0,
+        Category=1,
+        Author=2
+    }
+
+
+    public class SearchBooksResponseDto
+    {
+        private List<Book> _books;
+
+        public SearchBooksResponseDto(List<Book> books)
+        {
+            this._books = books;
+        }
+
+        public long TotalItems { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public object Data { get; set; }
+        public string Html { get; set; }
+
+        public List<Book> GetBooks()
+        {
+            return this._books;
+        }
+    }
+}
